c75b9277657629108c1ac61eb259c23a53fa7c53
jolivares@efferenthealth.com <jolivares@efferenthealth.com>
2023-02-15 22:31:12 -0500

    updated AssemblyInfo.cs

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,9 +1,3 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
 [assembly: AssemblyConfiguration("Release")]
-#endif
